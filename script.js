45d030184c541d2d454d78a26ad761d491f4afd6
Warley Ramires <warleyramiresgoncalves@gmail.com>
2024-12-10 21:11:02 -0300

    style(main): � layout do container de imagens

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function toggleMenu() {
     menuList.style.maxHeight = "0px";
   }
 }
+
+function openModal(imageSrc, title, description) {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("modal-image").src = imageSrc;
+    document.getElementById("modal-title").innerText = title;
+    document.getElementById("modal-description").innerText = description;
+  }
+  
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
+  const cards = document.querySelectorAll(".box-image-info");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const imageSrc = card.querySelector("img").src;
+      const title = card.querySelector("h3").innerText;
+      const description = card.querySelector("p").innerText;
+  
+      openModal(imageSrc, title, description);
+    });
+  });
+  
