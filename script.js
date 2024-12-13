86d438e7fa8d33bddf0d0ddc2675ad49c112ab84
Warley Ramires <warleyramiresgoncalves@gmail.com>
2024-12-12 22:45:04 -0300

    feat (main): ✨ correção de estilos e modal do formulário

--- script.js
+++ script.js
@@ -10,74 +10,84 @@ function toggleMenu() {
 }
 
 function openModal(imageSrc, title, description) {
-    document.getElementById("modal").style.display = "flex";
-    document.getElementById("modal-image").src = imageSrc;
-    document.getElementById("modal-title").innerText = title;
-    document.getElementById("modal-description").innerText = description;
-  }
-  
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
-  }
-  
-  const cards = document.querySelectorAll(".box-image-info");
-  cards.forEach((card) => {
-    card.addEventListener("click", () => {
-      const imageSrc = card.querySelector("img").src;
-      const title = card.querySelector("h3").innerText;
-      const description = card.querySelector("p").innerText;
-  
-      openModal(imageSrc, title, description);
-    });
+  document.getElementById("modal-cards").style.display = "flex";
+  document.getElementById("modal-image").src = imageSrc;
+  document.getElementById("modal-title").innerText = title;
+  document.getElementById("modal-description").innerText = description;
+}
+
+function closeModal() {
+  document.getElementById("modal-cards").style.display = "none";
+}
+
+const cards = document.querySelectorAll(".box-image-info");
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const imageSrc = card.querySelector("img").src;
+    const title = card.querySelector("h3").innerText;
+    const description = card.querySelector("p").innerText;
+
+    openModal(imageSrc, title, description);
   });
-  
+});
 
-  const form = document.getElementById('form-contato')
+const form = document.getElementById("form-contato");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-  form.addEventListener('submit', function(e){
-    e.preventDefault()
+  const nomeInput = document.getElementById("nome");
+  const sobrenomeInput  = document.getElementById("sobrenome").value;
+  const emailInput  = document.getElementById("email").value;
+  const telefoneInput  = document.getElementById("telefone").value;
+  const inputResultado = document.getElementById("resultado");
+  const resultado = parseInt(inputResultado.value);
+  const tagNum1 = document.getElementById("num1");
+  const tagNum2 = document.getElementById("num2");
+  const num1 = parseInt(tagNum1.innerText);
+  const num2 = parseInt(tagNum2.innerText);
 
-    const nome = document.getElementById("nome").value;
-    const sobrenome = document.getElementById("sobrenome").value;
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
+  const nome = nomeInput.value
+  const sobrenome = sobrenomeInput.value
+  const email = emailInput.value
+  const telefone = telefoneInput.value
 
-    const inputResultado = document.getElementById("resultado").value
-    const resultado = parseInt(inputResultado)
+  console.log(num1, num2);
+  console.log(tagNum1, tagNum1);
+  console.log()
+  let booleanResult = verificaResultado(num1, num2, resultado);
+  console.log(booleanResult);
 
-    const tagNum1 = document.getElementById("num1")
-    const tagNum2 = document.getElementById("num2")
-    console.log(tagNum1, tagNum1);
-    const num1 = parseInt(tagNum1.innerText);
-    const num2 = parseInt(tagNum2.innerText);
-    
-    console.log(nome, sobrenome, email, telefone, typeof(resultado));
-    console.log(num1, num2);
-    let booleanResult = verificaResultado(num1, num2, resultado)
-    console.log(booleanResult)
-
-    if(!booleanResult){
-      trocaNumeros(tagNum1, tagNum2)
-    }else{
-      console.log("não faz nada")
-    }
-
-  })
-
-
-  function verificaResultado(num1, num2, resultado){
-    if(num1+num2 === resultado){
-      return true
-    }else{
-      return false;
-    }
+  if (!booleanResult) {
+    trocaNumeros(tagNum1, tagNum2);
+    inputResultado.style.border = "2px solid red";
+    inputResultado.style.backgroundColor = "#ff95003f";
+  } else {
+    showSuccessModal();
   }
+});
 
-  function trocaNumeros(tagNum1, tagNum2){
-
-    var novoNumero1 = Math.floor(Math.random() * 999) + 1;
-    var novoNumero2 = Math.floor(Math.random() * 999) + 1;
-
-    tagNum1.innerText = novoNumero1;
-    tagNum2.innerText = novoNumero2;
+function verificaResultado(num1, num2, resultado) {
+  if (num1 + num2 === resultado) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+function trocaNumeros(tagNum1, tagNum2) {
+  var novoNumero1 = Math.floor(Math.random() * 999) + 1;
+  var novoNumero2 = Math.floor(Math.random() * 999) + 1;
+
+  tagNum1.innerText = novoNumero1;
+  tagNum2.innerText = novoNumero2;
+}
+
+function showSuccessModal() {
+  const modal = document.getElementById("success-modal");
+  modal.style.display = "flex";
+}
+
+function closeSuccessModal() {
+  const modal = document.getElementById("success-modal");
+  modal.style.display = "none";
+}
